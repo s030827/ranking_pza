ba0a2c2b61357e2af35e720a7cf9c819973f62b8
kondor <konrad.ociepka@netguru.pl>
2018-11-04 17:08:09 +0100

    Temporary commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@ require 'elasticsearch/model'
 
 class User < ApplicationRecord
   include Elasticsearch::Model
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates_presence_of :name, :surname
@@ -11,16 +9,74 @@ class User < ApplicationRecord
   has_many :ascents
   has_many :lines, through: :ascents
 
+  scope :men, -> { where(sex: 'M') }
+  scope :women, -> { where(sex: 'F') }
+
+  mapping do
+    indexes :ascents, type: 'nested' do
+      indexes :kind
+      indexes :style
+      indexes :line do
+        indexes :grade
+      end
+    end
+  end
+
+  def as_indexed_json(options = {})
+    self.as_json(
+      options.merge(
+        only: [:id, :name, :surname, :sex, :email, :city, :zip],
+        include: {
+          ascents: {
+            only: [:kind, :style],
+            include: { line: { only: :grade } }
+          }
+        }
+      )
+    )
+  end
+
+  def partner_search(query)
+    self.search({
+      query: {
+        bool: {
+          must: [
+          {
+            multi_match: {
+              query: query,
+              fields: [:author, :title, :body, :tags]
+            }
+          },
+          {
+            match: {
+              published: true
+            }
+          }]
+        }
+      }
+    })
+  end
+
   def full_name
     "#{name} #{surname}"
   end
 
-  def self.men
-    where(sex: 'M')
+  def users_who_made_8b_os
+    # {
+    #   "query": {
+    #       "nested" : {
+    #           "path" : "ascents",
+    #           "query" : {
+    #               "bool" : {
+    #                   "must" : [
+    #                   { "match" : {"ascents.kind": "s"} },
+    #                   { "match" : {"ascents.style": "os"} },
+    #                   { "match" : {"ascents.line.grade": "8b"} }
+    #                   ]
+    #               }
+    #           }
+    #       }
+    #   }
+    # }
   end
-
-  def self.women
-    where(sex: 'F')
-  end
-
 end
