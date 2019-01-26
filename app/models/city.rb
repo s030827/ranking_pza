dfdcdbb27881cebbaff0bd307d1e5c70205a25fa
kondor <konrad.ociepka@netguru.pl>
2019-01-26 13:01:52 +0100

    Add elasticsearch to City

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,3 +1,31 @@
 class City < ApplicationRecord
+  include Elasticsearch::Model
+
   has_and_belongs_to_many :zips
+  belongs_to :country
+
+  settings index: {
+    analysis: {
+      analyzer: {
+        analyzer_startswith: {
+          tokenizer: 'keyword',
+          filter: 'lowercase'
+        }
+      }
+    }
+  } do
+    mapping do
+      indexes :name, analyzer: 'analyzer_startswith'
+      indexes :country
+      indexes :zips_count, type: 'integer'
+    end
+  end
+
+  def as_indexed_json(options = {})
+    {
+      name: name,
+      country: country.iso,
+      zips_count: zips.count
+    }
+  end
 end
